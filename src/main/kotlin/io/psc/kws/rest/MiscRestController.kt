3b2a6b3a07dfaff03d0c7c8b947a47f538579103
sykq <genericmailacc@gmail.com>
2020-02-19 19:45:46 +0100

    misc

--- src/main/kotlin/io/psc/kws/rest/MiscRestController.kt
+++ src/main/kotlin/io/psc/kws/rest/MiscRestController.kt
@@ -18,13 +18,32 @@ class MiscRestController {
      * Generates the given number [iterations] of random integers between 1 and 100 and groups them by number
      * of occurrences into a Map.
      */
-    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun getMisc(@RequestParam iterations: Int?): Map<String, Any> {
+    @GetMapping(path = ["clumsyRandoms"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getRandomsClumsily(@RequestParam iterations: Int?): Map<String, Any> {
         val randomNumbers = List(iterations ?: 1000) { Random.Default.nextInt(1, 101) }
                 .stream()
                 .collect({ mutableMapOf<Int, Int>() },
                         { accumulated, current -> accumulated.merge(current, 1) { a, _ -> a + 1 } },
                         { a, b -> a.mapValues { it.value + b[it.key]!! } })
+
+        return mutableMapOf("randoms" to randomNumbers.toSortedMap(),
+                "totalIterations" to randomNumbers.values.reduce { a, b -> a + b })
+    }
+
+    /**
+     * Generates the given number [iterations] of random integers between 1 and 100 and groups them by number
+     * of occurrences into a Map.
+     */
+    @GetMapping(path = ["randomInts"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getRandoms(@RequestParam iterations: Int?): Map<String, Any> {
+        val randomNumbers = List(iterations ?: 1000) { Random.Default.nextInt(1, 101) }.groupingBy { it }
+                .aggregate { _, accumulator: Int?, _, first ->
+                    if (first) {
+                        1
+                    } else {
+                        accumulator?.plus(1)
+                    }
+                }.mapValues { it.value ?: 0 }
 
         return mutableMapOf("randoms" to randomNumbers.toSortedMap(),
                 "totalIterations" to randomNumbers.values.reduce { a, b -> a + b })
