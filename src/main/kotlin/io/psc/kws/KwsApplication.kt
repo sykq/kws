9510e11b6eb4c8cb37910762bd4316cf39ec8aa7
sykq <genericmailacc@gmail.com>
2020-03-07 14:03:11 +0100

    spring cloud gateway with kotlin beans-dsl

--- src/main/kotlin/io/psc/kws/KwsApplication.kt
+++ src/main/kotlin/io/psc/kws/KwsApplication.kt
@@ -1,11 +1,50 @@
 package io.psc.kws
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.boot.autoconfigure.web.ServerProperties
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.support.beans
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
+import reactor.core.publisher.Mono
 
 @SpringBootApplication
 class KwsApplication
 
 fun main(args: Array<String>) {
-	runApplication<KwsApplication>(*args)
+    SpringApplicationBuilder()
+            .sources(KwsApplication::class.java)
+            .initializers(
+                    beans {
+                        bean {
+                            val builder = ref<RouteLocatorBuilder>()
+                            builder.routes().route("tw_redirect") {
+                                        it.path("/tw").uri("https://www.twitch.tv/")
+                                    }
+                                    .build()
+                        }
+                        bean {
+                            // WebFilter to add the server context-path to a request's url
+                            val contextPath = ref<ServerProperties>().servlet.contextPath
+                            WebFilter { exchange, chain ->
+                                val request = exchange.request
+                                if (request.uri.path.startsWith(contextPath)) {
+                                    chain.filter(
+                                            exchange.mutate()
+                                                    .request(
+                                                            request.mutate()
+                                                                    .contextPath(contextPath)
+                                                                    .build())
+                                                    .build())
+                                } else {
+                                    chain.filter(exchange)
+                                }
+                            }
+                        }
+                    }
+            )
+            .run(*args)
+//    runApplication<KwsApplication>(*args)
 }
